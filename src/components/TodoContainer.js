39491ad07000fa78938e975aeed34105dbd2f72e
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-06 19:00:18 +0200

    Add main component

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,13 @@
+import React from "react";
+class TodoContainer extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>First boilerplate todo</h1>
+        <p>inside a react component</p>
+      </div>
+    )
+  }
+}
+
+export default TodoContainer
