faed8ebbd17b01a0cefb761bcd576ad9da9994fa
mutinhiri <barnabmutinhiri@gmail.com>
2021-12-06 20:58:09 +0200

    Add header as an arrow function for React

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,7 @@
 import React from "react";
 import TodoList from "./TodoList";
+import { Header } from "../Header";
+
 class TodoContainer extends React.Component {
   state =  {
     todos: [
@@ -23,6 +25,7 @@ class TodoContainer extends React.Component {
   render() {
     return (
       <div>
+        <Header />
         <TodoList todos = {this.state.todos} />
       </div>
     )
